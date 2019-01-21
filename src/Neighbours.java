20020ee8df19e26c846dffd1a0439b4d6b79916e
Jakod <jakob.wik2@gmail.com>
2019-01-21 16:18:31 +0100

    test

--- src/Neighbours.java
+++ src/Neighbours.java
@@ -103,7 +103,7 @@ public class Neighbours extends Application {
         // % of surrounding neighbours that are like me
         double threshold = 0.7;
         Actor[][] newWorld;
-out.println("asda");
+out.println("asdad");
         // TODO update the world
     }
 
